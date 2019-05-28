052d45cc878a1d9258c2f633ba666859bf667526
vlad chub <vlad.chub@gmail.com>
2019-05-28 14:08:09 -0700

    mul

--- src/sicp/ch2/poly/poly.clj
+++ src/sicp/ch2/poly/poly.clj
@@ -22,9 +22,8 @@
           (p [t] (first t))
           (c [t] (second t))
           (add-t [x y] (if (= (p x) (p y))
-                         [(p x) (+ (c x) (c y))]
-                         (throw (Exception. (str "Can't add terms " x y))))
-            )
+                         [(p x) (arith/add (c x) (c y))]
+                         (throw (Exception. (str "Can't add terms " x y)))))
           (add-terms [xs ys]
                      (cond
                        (empty? xs) ys
@@ -32,19 +31,31 @@
                        (< (p (first xs)) (p (first ys))) (cons (first xs) (add-terms (rest xs) ys))
                        (> (p (first xs)) (p (first ys))) (cons (first ys) (add-terms xs (rest ys)))
                        :else (cons (add-t (first xs) (first ys)) (add-terms (rest xs) (rest ys)))))
+
           (add [x y] (if (same-v x y)
                        (make (:v x) (add-terms (:terms x) (:terms y)))
                        (throw (Exception. (str "Can't add Poly with different vars " (:v x) " " (:v y))))))
-          ;; (mul-t [p t])
-          ]
 
+          (mul-t [t ts] (map #(list (+ (p %) (p t)) (* (c %) (c t))) ts))
+
+          (mul [x y] (if (same-v  x y)
+                       (make (:v x)
+                             (reduce add-terms
+                                     (map #(mul-t % (:terms y)) (:terms x))
+                                     ))
+                       (throw (Exception. (str "Can't add terms " x y)))))]
+
+    (put 'raise '(poly) (fn [x] nil))
     (put 'make 'poly make)
     (put 'add '(poly poly) add)
+    (put 'mul '(poly poly) mul)
+    (put 'mul-t '(term poly) mul-t)
     'done))
 
 (install-poly)
 
 (defn poly [v terms] ((get-in @arith/f-tbl '(make poly)) v terms))
+(defn mul-t [t ts] ((get-in @arith/f-tbl '(mul-t (term poly))) t ts))
 ;; (defn add [x y] (apply-genric 'add x y))
 
 (testing
@@ -55,7 +66,12 @@
    (is (thrown? Exception  (arith/add p (poly 'y '((0 2))))))
    (is (= '((0 3)) (:terms (arith/add (poly 'x '((0 2))) (poly 'x '((0 1)))))))
    (is (= '((0 4) (1 1) (2 2)) (:terms (arith/add  p p2))))
-   ;; (is (= 1 p))
+
+   (is (= '((1 2)) (mul-t  '(0 1) '((1 2)))))
+   (is (= '((2 4) (3 4)) (mul-t  '(1 2) '((1 2) (2 2)))))
+   (is (= (poly 'x '((0 2) (2 1))) (arith/mul p (poly 'x '((0 1))))))
+   (is (= (poly 'x '((1 2) (2 4) (3 1) (4 2))) (arith/mul p (poly 'x '((1 1) (2 2))))))
+
    ))
 
 ;; (swap! f-tbl assoc :b 1)
@@ -69,9 +85,10 @@
   (letfn [(f [xs] (if (empty? xs) xs (cons (first xs) (f (rest xs)))))]
     (f [3 2]))
 
-  (meta (let [t ^:hi (Term. 1 2)]
+  (meta (let [t ^:hi (->Term 1 2)]
           t))
   (meta ^{:t 'hi} [1 2])
+  (meta ^{:t 'hi-hi} {1 2})
   (meta ^{:t 'hi} (Term. 1 2))
-  (< nil 0)
-  )
+
+  (< nil 0))
