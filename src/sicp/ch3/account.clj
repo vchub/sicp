95db4911ae3b3799a132d69370c119650c64dea4
vlad chub <vlad.chub@gmail.com>
2019-05-30 07:54:37 -0700

    account with password

--- src/sicp/ch3/account.clj
+++ src/sicp/ch3/account.clj
@@ -3,32 +3,96 @@
 
 (defrecord Account [b w d])
 
-(defn make-account
+(defn make-account0
   "num -> [->num, f, f]
    f: (num)->num"
   [balance]
   (let [state (atom balance)
-        bal (fn[] @state)
+        bal (fn [] @state)
         withdraw (fn withdraw [x] (if (<= x @state)
                                     (do
-                                    (swap! state - x)
-                                    @state)
+                                      (swap! state - x)
+                                      @state)
                                     "not enough funds"))
-        deposit (fn[x] (swap! state + x))]
+        deposit (fn [x] (swap! state + x))]
     (Account. bal withdraw deposit)))
 
 (testing
-  (let [a (make-account 10)]
-    (is (= 10 ((:b a))))
-    (is (= 5 ((:w a) 5)))
-    (is (= "not enough funds" ((:w a) 6)))
-    (is (= 15 ((:d a) 10)))
-    (is (= 0 ((:w a) 15)))
+ (let [a (make-account0 10)]
+   (is (= 10 ((:b a))))
+   (is (= 5 ((:w a) 5)))
+   (is (= "not enough funds" ((:w a) 6)))
+   (is (= 15 ((:d a) 10)))
+   (is (= 0 ((:w a) 15)))))
+
+(defn make-monitored
+  "fn -> fn
+  (f arg) = (fn arg)
+  (f 'how-many-calls?) -> num of calls"
+  [f]
+  (let [cnt (atom 0)]
+    (fn [arg]
+      (if (= arg 'how-many-calls?)
+        @cnt
+        (do (swap! cnt inc)
+            (f arg))))))
+
+(testing
+ (let [sq (make-monitored #(* % %))]
+   (is (= 0 (sq 'how-many-calls?)))
+   (is (= 4 (sq 2)))
+   (is (= 1 (sq 'how-many-calls?)))
+   (is (= 1 (sq 'how-many-calls?)))))
+
+(defn make-account
+  "psw, balance -> psw, message -> num"
+  [psw b]
+  (let [b (atom b)
+        cnt (atom 0)
+        withdraw (fn [x] (if (<= x @b)
+                           (do (swap! b - x)
+                               @b)
+                           "insufficient funds"))
+        deposit (fn [x] (swap! b + x) @b)
+
+        call-cops (fn [] (prn "call-cops"))
+
+        check-psw (fn [ps] (if (= psw ps)
+                             (do (swap! cnt (fn[x] 0)) true)
+                             (do (swap! cnt inc)
+                                 (when (> @cnt 2) (call-cops))
+                                 false)))
+
+        dispatch (fn [ps message]
+                   (if (check-psw ps)
+                     (condp = message
+                       'withdraw withdraw
+                       'deposit deposit
+                       :else (throw (Exception. (str "unknown request MAKE-ACCOUNT message " message))))
+                     (fn[x] "Incorrect password")))]
+    dispatch ))
+
+(testing
+  (let [a (make-account 'pass 5)]
+    (is (= 4 ((a 'pass 'withdraw) 1)))
+    (is (= "Incorrect password" ((a 'x 'withdraw) 1)))
+    (is (= "Incorrect password" ((a 'x 'withdraw) 1)))
+    ;; (is (= "Incorrect password" ((a 'x 'withdraw) 1)))
+    (is (= 3 ((a 'pass 'withdraw) 1)))
+    (is (= 6 ((a 'pass 'deposit) 3)))
     ))
 
 (comment
-  (defrecord T[t s])
+  (defrecord T [t s])
   (meta ^{:t :hi} 's)
   (meta (with-meta 's {:t :hi}))
-  (meta (with-meta (T. 1 2) {:t :hi}))
-  )
+  (meta (with-meta (T. 1 2) {:t :hi})))
+
+;; tranducers
+(def xform (comp (map inc) (filter even?)))
+
+(testing
+ (is (= [2 4] (into [] xform (range 4))))
+  (is (= [2 4] (sequence xform (range 4))))
+  (is (= 6 (transduce xform + (range 4))))
+  (is (= 7 (transduce xform + 1 (range 4)))))
