95a33e4f89dffbdc0363310e52e39bcc06ab6b82
vlad chub <vlad.chub@gmail.com>
2019-06-02 15:14:37 -0700

    set-h!

--- src/sicp/ch3/fun_pair.clj
+++ src/sicp/ch3/fun_pair.clj
@@ -4,18 +4,27 @@
 ;; Pair [h t] or [car cdr]
 
 (defn cns [h t]
-  (fn dispatch [message]
-    (cond
-      (= 'h message) h
-      (= 't message) t
-      :else (throw (Exception. (str "Undefined operation on CNS " message))))))
+  (let [h (atom h)
+        t (atom t)]
+    (fn dispatch [msg]
+      (cond
+        (= 'set-h! msg) (fn [x] (reset! h x))
+        (= 'set-t! msg) (fn [x] (reset! t x))
+        (= 'h msg) @h
+        (= 't msg) @t
+        :else (throw (Exception. (str "Undefined operation on CNS " msg)))))))
 
 (defn h [pair] (pair 'h))
+(defn set-h! [pair x] ((pair 'set-h!) x))
 (defn t [pair] (pair 't))
+(defn set-t! [pair x] ((pair 'set-t!) x))
 
 (testing
  (let [l1 (cns 1 nil)
        l2 (cns 2 l1)]
    (is (= 2 (h l2)))
    (is (= l1 (t l2)))
-   (is (= 1 (h (t l2))))))
+   (is (= 1 (h (t l2))))
+   (set-t! l1 3)
+   (set-h! l2 '(1 2))
+   (is (= '(1 2) (h l2)))))
