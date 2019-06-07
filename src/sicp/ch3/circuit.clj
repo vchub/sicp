f76f869ea85633910b98f25d157fa9ba6bd61b64
vlad chub <vlad.chub@gmail.com>
2019-06-07 10:00:17 -0700

    adder

--- src/sicp/ch3/circuit.clj
+++ src/sicp/ch3/circuit.clj
@@ -1,7 +1,7 @@
 (ns sicp.ch3.circuit
   (:require [clojure.test :refer :all]
             [clojure.core.async :as a :refer [<! >! <!! >!! go chan timeout]])
-  (:import (java.util.concurrent ScheduledThreadPoolExecutor TimeUnit)))
+  )
 
 (defn after-delay [dt f] (fn [] (go
                                   (<! (timeout dt))
@@ -9,18 +9,20 @@
 
 (defn b-not [x] {:pre [(#{0 1} x)]} (bit-flip x 0))
 
-(defrecord Wire [state action])
+(defrecord Wire [state actions])
 
 (defn nil-f [])
-(defn make-wire [] (atom (Wire. 0 nil-f)))
+(defn make-wire [] (atom (Wire. 0 nil)))
 (defn sig [w] (:state @w))
-;; (defn sig! [w x] (when (not= (sig w) x)
-;;                    (swap! w assoc :state x)
-;;                    ((:action @w))))
-(defn sig! [w x] (swap! w assoc :state x)
-  ((:action @w)))
+(defn sig! [w x] (when (not= (sig w) x)
+                   (swap! w assoc :state x)
+                   (doseq [act (:actions @w)]
+                     (act))
+                   ))
+;; (defn sig! [w x] (swap! w assoc :state x)
+;;   ((:action @w)))
 
-(defn add-act! [w f] (swap! w assoc :action f))
+(defn add-act! [w f] (swap! w update :actions conj f))
 
 ;; DELAYS
 (def inverter-delay 1)
@@ -43,6 +45,80 @@
     (act)
     (add-act! a (after-delay and-delay act))
     (add-act! b (after-delay and-delay act))))
+
+(defn half-adder [a b s c]
+  (let [d (make-wire)
+        e (make-wire)]
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s))
+  )
+
+(defn adder [a b c-in s c-out]
+  (let [c1 (make-wire)
+        c2 (make-wire)
+        s1 (make-wire)
+        ]
+    (half-adder b c-in s1 c1)
+    (half-adder a s1 s c2)
+    (or-gate c1 c2 c-out)
+    )
+  )
+
+(deftest test-adders
+(testing "adder"
+    (let [a (make-wire)
+          b (make-wire)
+          c-in (make-wire)
+          s (make-wire)
+          c-out (make-wire)
+          gate (adder a b c-in s c-out)]
+
+      (sig! a 1)
+      (Thread/sleep (* 5 and-delay))
+      (is (= 1 (sig s)))
+      (is (= 0 (sig c-out)))
+
+      (sig! b 1)
+      (Thread/sleep (* 8 and-delay))
+      (is (= 0 (sig s)))
+      (is (= 1 (sig c-out)))
+
+      (sig! c-in 1)
+      (Thread/sleep (* 10 and-delay))
+      (is (= 1 (sig s)))
+      (is (= 1 (sig c-out)))
+  ))
+
+  (testing "half-adder"
+    (let [a (make-wire)
+          b (make-wire)
+          c (make-wire)
+          s (make-wire)
+          gate (half-adder a b s c)]
+      (is (= 0 (sig c)))
+      (is (= 0 (sig s)))
+
+      (sig! a 1)
+      ;; (sig! b 0)
+      (Thread/sleep (* 3 and-delay))
+      (is (= 1 (sig s)))
+      (is (= 0 (sig c)))
+
+      (sig! b 1)
+      (Thread/sleep (* 4 and-delay))
+      (is (= 0 (sig s)))
+      (is (= 1 (sig c)))
+
+      (sig! a 0)
+      (Thread/sleep (* 4 and-delay))
+      (is (= 1 (sig s)))
+      (is (= 0 (sig c)))
+  ))
+  )
+
+(test-adders)
 
 (deftest run-test
 
@@ -103,7 +179,7 @@
 
       (sig! a 0)
       ;; (is (= 0 (sig b)))
-      (Thread/sleep (* 2 inverter-delay))
+      (Thread/sleep (* 3 inverter-delay))
       (is (= 1 (sig b)))))
 
   (testing "wire"
@@ -117,12 +193,15 @@
       (sig! a 1)
       (is (= 1 (sig a)))
       (is (= 1 @cnt))
+
       (sig! a 1)
       (is (= 1 (sig a)))
+      (is (= 1 @cnt))
+
+      (sig! a 0)
+      (is (= 0 (sig a)))
       (is (= 2 @cnt))
-      ;; (sig! a 0)
-      ;; (is (= 0 (sig a)))
-      ;; (is (= 2 @cnt))
+
       (add-act! a delayed-act)
       (sig! a 1)
       (is (= 1 (sig a)))
@@ -130,7 +209,7 @@
       ;; (after-delay 100 (fn[]
       ;;                    (is (= 1 2))))
       (Thread/sleep 2)
-      (is (= 3 @cnt))))
+      (is (= 4 @cnt))))
 
   (testing "bitwise"
     (are [x y] (= x y)
