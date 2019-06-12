ac183e9cff9ca36a89501f9feec3b9db40de4267
vlad chub <vlad.chub@gmail.com>
2019-06-12 13:40:38 -0700

    ex3_4

---
+++ src/sicp/ch3/concurency/bank.clj
@@ -0,0 +1,15 @@
+(ns sicp.ch3.concurency.bank
+  (:require [clojure.test :refer :all]
+            [clojure.math.combinatorics :as comb]))
+
+(defn ex3_4 []
+  (let [b 100
+        procs [(fn [b] (+ b 10)) (fn [b] (- b 20)) (fn [b] (/ b 2))]
+        xform (fn [fs] (reduce #(%2 %1) b fs))]
+    (map xform (comb/permutations procs))))
+
+(testing "ex3.4"
+  (is (= (sort [35 40 45 50]) (sort (set (ex3_4))))))
+
+(comment
+  (comb/permutations [1 2]))
