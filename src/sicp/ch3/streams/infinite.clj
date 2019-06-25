ff5920f6644ad4147022d47c2d9d6797eb5e4745
vlad chub <vlad.chub@gmail.com>
2019-06-25 14:31:04 -0700

    fibonacci fibs

--- src/sicp/ch3/streams/infinite.clj
+++ src/sicp/ch3/streams/infinite.clj
@@ -12,8 +12,7 @@
 
 (defn to-vector "Stream -> vector"
   [s]
-  (if (nil? s) [] (conj (to-vector (cdr s)) (car s)))
-  )
+  (if (nil? s) [] (conj (to-vector (cdr s)) (car s))))
 
 (defn n-primes
   "num -> Stream
@@ -25,8 +24,7 @@
       ;; (prn factors)
       (if (factorized? n factors)
         (recur (dec n))
-        (cons-m n (n-primes (dec n)))
-        ))))
+        (cons-m n (n-primes (dec n)))))))
 
 (defn nums [start step] (cons-m start (nums (+ start step) step)))
 (def s-nums (nums 5 2))
@@ -38,8 +36,8 @@
   (cons-m (car s)
           (do
             ;; (prn (car s))
-          (sieve (filter-stream #(not= 0 (rem % (car s)))
-                         (cdr s))))))
+            (sieve (filter-stream #(not= 0 (rem % (car s)))
+                                  (cdr s))))))
 
 (def primes (sieve (nums 2 1)))
 
@@ -49,17 +47,33 @@
 ;;                       (fn[x] (not (factorized? x (to-vector (filter-stream #(< % (dec x)) primes)))))
 ;;                       s-nums))))
 
+(def ones (cons-m 1 ones))
+
+(defn add-streams [s1 s2] (ss/map-streams + s1 s2))
+
+(def integers (cons-m 1 (add-streams ones integers)))
+
+(def fibs (cons-m 0 (cons-m 1 (ss/map-streams + fibs (cdr fibs)))))
+
 (deftest test-n-primes
+
+  (testing "fibs fibonacci"
+    (is (= [0 1 1 2 3 5 8] (take-s fibs 7))))
+
   (testing "implicit Stream"
-    (let [ones ((fn ones [] (cons-m 1 (ones))))]
     (is (= [1 1 1] (ss/take-s ones 3)))
-    (is (= (repeat 4 1) (ss/take-s ones 4)))
-    ))
+    (is (= [2 2 2] (ss/take-s (ss/map-streams + ones ones) 3)))
+    (is (= [2 2 2] (ss/take-s (add-streams ones ones) 3)))
+    (is (= [1 2 3] (ss/take-s integers 3)))
+    (is (= (range 1 11) (ss/take-s integers 10)))
+
+    (let [ones ((fn ones [] (cons-m 1 (ones))))]
+      (is (= [1 1 1] (ss/take-s ones 3)))
+      (is (= (repeat 4 1) (ss/take-s ones 4)))))
 
   (testing "fibonacci"
     (let [xs (fibgen 1 1)]
-    (is (= [1 1 2 3 5 8] (take-s xs 6))))
-    )
+      (is (= [1 1 2 3 5 8] (take-s xs 6)))))
 
   (testing "primes"
     (is (= [2 3] (take-s primes 2)))
@@ -67,24 +81,19 @@
     (is (= [2 3 5 7] (take-s primes 4)))
     (is (= [2 3 5 7 11 13] (take-s primes 6)))
     (is (= 5 (ss/stream-ref primes 2)))
-    (is (= 233 (ss/stream-ref primes 50)))
-    )
+    (is (= 233 (ss/stream-ref primes 50))))
 
   (testing "nums"
     (let [xs (nums 1 2)]
-    (is (= [1 3 5] (take-s xs 3))))
-    )
+      (is (= [1 3 5] (take-s xs 3)))))
 
   (testing "to-vector"
-    (is (= [ 1] (to-vector (cons-m 1 nil))))
-    (is (= [ 2 1 ] (to-vector (cons-m 1 (cons-m 2 nil)))))
-    )
+    (is (= [1] (to-vector (cons-m 1 nil))))
+    (is (= [2 1] (to-vector (cons-m 1 (cons-m 2 nil))))))
 
   (testing "n-primes"
     (is (= [2 3] (to-vector (n-primes 3))))
     (is (= [2 3 5] (to-vector (n-primes 5))))
-    (is (= [2 3 5 7 11] (to-vector (n-primes 12))))
-    )
-  )
+    (is (= [2 3 5 7 11] (to-vector (n-primes 12))))))
 
 (test-n-primes)
