d81e833ad4562bfb2985dd92c9edf83ed47a6278
vlad chub <vlad.chub@gmail.com>
2019-07-01 14:39:44 -0700

    pythagorean-tripple-map

--- src/sicp/ch3/streams/pairs.clj
+++ src/sicp/ch3/streams/pairs.clj
@@ -55,13 +55,89 @@
            (ss/map-stream #(vector % (car t)) (cdr s))
            (all-pairs (cdr s) (cdr t)))))
 
+(defn tripples
+  "Stream, Stream, Stream -> Stream of [ai bj ck]
+  i<=j<=k"
+  [a b c]
+  (cons-m [(car a) (car b) (car c)]
+          (interleave-s
+           (ss/map-stream (fn [[b c]] [(car a) b c]) (pairs (cdr b) (cdr c)))
+           (tripples (cdr a) (cdr b) (cdr c))))
+  ;; (ss/map-stream (fn [[a [b c]]] [a b c]) (pairs a (pairs b c)))
+  )
+
+(defn square [x] (* x x))
+
+(defn hypoth [a b] (+ (square a) (square b)))
+
+(def naturals (cons-m 1 (ss/map-stream inc naturals)))
+
+(defn pythagorean-tripple "i^2 + j^2 = k^2 i<=j<=k"
+  []
+  (ss/filter-stream (fn [[a b c]] (= (hypoth a b) (square c)))
+                    (tripples naturals naturals naturals)))
+
+(defn pythagorean-tripple-map
+  "num -> (set [i j k])
+  i^2 + j^2 = k^2 i<=j<=k<= n"
+  [n]
+  (reduce
+   (fn [acc [i j]] (let [k (hypoth i j)
+                         k-root (int (Math/sqrt k))]
+                     (if (= k (square k-root))
+                       (conj acc [i j k-root])
+                       acc)))
+   #{}
+   (for [j (range 1 n)
+         i (range 1 (inc j))]
+     [i j])))
+
 (deftest test-pairs
+
+  (testing "ex 3.69"
+    (let [ts (tripples integers integers integers)
+          ts-100 (set (take-s ts 100))
+          exp (set [[0 0 0] [1 1 1] [1 2 2] [2 2 2] [0 0 2]])
+          errors (set [[1 0 0] [1 0 1] [1 2 1] [2 1 1] [2 0 2]])
+          ]
+
+      ;; (is (= [[0 0 0] [0 0 1] [1 0 1] [0 1 1]] (take-s ts 8)))
+      (is  (some #(= % [1 3 4]) ts-100))
+      (is  (some exp ts-100))
+      (is  (not (some errors ts-100)))
+      )
+
+    (let [
+          p-triple-map (pythagorean-tripple-map 100)
+          primitive-triples (set '((3, 4, 5) (5, 12, 13) (8, 15, 17) (7, 24, 25)
+                                             (20, 21, 29) (12, 35, 37) (9, 40, 41) (28, 45, 53)
+                                             (11, 60, 61) (16, 63, 65) (33, 56, 65) (48, 55, 73)
+                                             (13, 84, 85) (36, 77, 85) (39, 80, 89) (65, 72, 97)))
+          pyth-s (set (take-s (pythagorean-tripple) 4))
+          ]
+      ;; (prn (clojure.set/difference primitive-triples p-triple-map))
+      (is (clojure.set/subset? primitive-triples p-triple-map))
+      (is (clojure.set/subset? (set [[3 4 5] [5 12 13]]) pyth-s))
+      (is (not (clojure.set/subset? (set [[3 4 5] [5 12 13] [7 24 25]]) pyth-s)))
+      ;; (prn "pythagorean-tripple-map 100" (time (do (pythagorean-tripple-map 100) nil)))
+      ;; (prn "pythagorean-tripple 5" (time (take-s (pythagorean-tripple) 5)))
+      ))
+
+  (testing "ex 3.68"
+    ;; (let [pairs-2 (fn pairs-2 [s t]
+    ;;                 (interleave-s
+    ;;                  (ss/map-stream #(vector (car s) %) t)
+    ;;                  (pairs-2 (cdr s) (cdr t))))]
+    ;;   (is (= [[0 0] [1 1] [0 1] [1 2]]
+    ;;          (take-s (pairs-2 integers integers) 4)))
+    ;;   (is  (some #(= % [3 4]) (take-s (pairs-2 integers integers) 100))))
+    )
+
   (testing "ex 3.67"
     (let [all-pairs-for (for [x (range) y (range)] [x y])]
       (is (= [[0 0] [0 1] [0 2] [0 3]] (take 4 all-pairs-for)))
       (is (= [[0 0] [0 1] [1 0] [1 1] [0 2] [2 0] [1 2] [0 3] [3 0]]
-             (take-s (all-pairs integers integers) 9)))
-      ))
+             (take-s (all-pairs integers integers) 9)))))
 
   (testing "interleave-s"
     (let [i1 (reduce #(cons-m %2 %1) nil (range 10 -1 -1))]
@@ -115,3 +191,8 @@
       (is (= (take 10 int-pairs-for) (take 10 ps-cat))))))
 
 (test-pairs)
+
+(comment
+  ((set '(1 2)) [2])
+  (some (set '(1 2)) (set [2]))
+  (some (set '(1 2)) (set [0 3])))
