9dd279d6213c65b3649faab84b581c153515646d
vlad chub <vlad.chub@gmail.com>
2019-07-15 07:47:34 -0700

    apply-f

--- src/sicp/ch4/eval_data_multi.clj
+++ src/sicp/ch4/eval_data_multi.clj
@@ -6,6 +6,92 @@
 
 (def primitive-proc-symbols #{'+ '- '* '/ '< '> '=})
 
+(defn make-env [] (atom (-> (into {} (map #(vector % (eval %))) primitive-proc-symbols)
+                            ;; (assoc 'true true)
+                            ;; (assoc 'false false)
+                            )))
+
+(defmulti eval-f (fn [exp env]
+                   (cond
+                     (or (number? exp) (string? exp) (boolean? exp)) 'self-eval
+                     (symbol? exp) 'symbol
+                     :else (first exp))))
+
+(defmethod eval-f 'self-eval [exp env] exp)
+(defmethod eval-f 'symbol [exp env] (get @env exp))
+(defmethod eval-f 'def-f! [exp env] (swap! env  assoc (second exp) (eval-f (nth exp 2) env)))
+(defmethod eval-f 'fn [exp env] exp)
+(defmethod eval-f 'if [exp env] (if (eval-f (second exp) env)
+                                  (eval-f (nth exp 2) env)
+                                  (eval-f (nth exp 3) env)))
+(defmethod eval-f :default [exp env] (apply-f
+                                       ;; fn
+                                      (eval-f (first exp) env)
+                                       ;; args
+                                      (map #(eval-f % env) (next exp))
+                                      env))
+
+(def compound-proc? seq?)
+(defn proc-params [exp] (second exp))
+(defn proc-body [exp] (drop 2 exp))
+(defn extend-env [env params args]
+  (let [p-env (into @env (map vector params args))]
+    (atom p-env)))
+
+(defn apply-f
+  "proc is either '(fn[x y] ....) or primitive-proc"
+  [proc args env]
+  (if (compound-proc? proc)
+    (let [body (proc-body proc)
+          env (extend-env env (proc-params proc) args)
+          ret (reduce (fn [acc exp] (eval-f exp env)) nil body)]
+      ret)
+    (apply proc args)))
+
+(deftest test-multi-method
+  (testing "eval-f"
+    (let [env (make-env)
+          eval-f (fn [x] (eval-f x env))]
+      (is (= 1 (eval-f 1)))
+      (is (= "foo" (eval-f "foo")))
+      (is (= nil (eval-f 'x)))
+      (eval-f '(def-f! x 1))
+      ;; (prn env)
+      (is (= 1 (eval-f 'x)))
+      (is (= '(fn [x y] (+ x y)) (eval-f '(fn [x y] (+ x y)))))
+      ;; (is (= '(:apply-f nil (2)) (eval-f '(s 2))))
+      (is (= 5 (eval-f '(+ 2 3))))
+      (is (= 2/3 (eval-f '(/ 2 3))))
+
+      (is (= true (eval-f 'true)))
+      (is (= 2/3 (eval-f '(if true (/ 2 3) (* 1 4)))))
+      (is (= 2/3 (eval-f '(if "some" (/ 2 3) (* 1 4)))))
+      (is (= 4 (eval-f '(if (< 1 0) (/ 2 3) (* 1 4)))))
+
+      (eval-f '(def-f! sum (fn [x y] (+ x y))))
+      ;; (is (= '(:apply-f (fn [x y] (+ x y)) (2 3)) (eval-f '(s 2 3))))
+      (is (= 5 (eval-f '(sum 2 3))))
+      (is (= 10 (eval-f '(sum (sum 2 3) (sum 2 3)))))
+      (is (= 7 (eval-f '(sum (sum 2 3) (* 1 2)))))
+
+      ;; anonymous fn invocation and local def-f!
+      (is (= 3 (eval-f '((fn [] 3)))))
+      (is (= -1 (eval-f '((fn []
+                            (def-f! sum (fn [x y] (- x y)))
+                            (sum 2 3))))))
+      ;; in the main scope sum is the same
+      (is (= 5 (eval-f '(sum 2 3))))
+
+      ;; recursion
+      (eval-f '(def-f! factorial (fn [n] (if (< n 2)
+                                           1
+                                           (* n (factorial (- n 1)))))))
+      (is (= 1 (eval-f '(factorial 1))))
+      (is (= 2 (eval-f '(factorial 2))))
+      (is (= 6 (eval-f '(factorial 3)))))))
+
+(test-multi-method)
+
 (defprotocol Pair
   (h [_] "head")
   (t [_] "tail"))
@@ -24,22 +110,7 @@
   Pair
   {:h (fn [s] 'string)})
 
-(defmulti eval-f (fn [exp]
-                   (cond
-                     (or (number? exp) (string? exp)) 'self-eval
-                     (symbol? exp) 'symbol
-                     :else (first exp))))
-
-(defmethod eval-f 'self-eval [s] s)
-;; TODO: fix to var eval
-(defmethod eval-f 'symbol [s] s)
-
 (deftest test-data-multi
-  (testing "eval-f"
-    (is (= 1 (eval-f 1)))
-    (is (= "foo" (eval-f "foo")))
-    (is (= 'x (eval-f 'x))))
-
   (testing "protocol"
     (is (= 1 ({"foo" 1} "foo")))
     ;; (is (= clojure.lang.ISeq (class '())))
@@ -56,3 +127,7 @@
     ))
 
 (test-data-multi)
+
+(comment
+  ({"some" 1} "some")
+  ({"some" 1} :k))
