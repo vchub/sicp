f551798d7358eee1e9ddbe55992e0d2e707e6928
vlad chub <vlad.chub@gmail.com>
2019-11-03 00:13:33 -0700

    win paths

--- src/problems/probab/graph_path.clj
+++ src/problems/probab/graph_path.clj
@@ -2,11 +2,25 @@
   (:require [clojure.test :refer :all]))
 
 (defn win-seq
-  "num -> [0,1]
-   games to win -> path"
+  "num -> [[0,1]]
+   games to win -> paths"
   [n]
-  (if (zero? n) 1 2)
-  )
+  (letfn [(go [path a b]
+              (if (or (zero? a) (zero? b))
+                [path]
+                (let [pa (go (conj path 0) (dec a) b)
+                      pb (go (conj path 1) a (dec b))
+                      ret (concat pa pb)]
+                ;; (prn ret)
+                  ret)))]
+    (count (go [] n n))))
 
 (testing
-  (is (= [[0],[1]] (win-seq 1))) )
+;;  (is (= [[0],[1]] (win-seq 1)))
+ (is (= 2 (win-seq 1)))
+  (is (= 6 (win-seq 2)))
+  (is (= 20 (win-seq 3)))
+  )
+
+(comment
+  (* 3 4))
