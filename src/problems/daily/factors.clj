8ee649e42e1244df0f5a07eecfd0ecf752c3d4a8
vlad chub <vlad.chub@gmail.com>
2019-11-28 18:00:18 -0800

    max-len interview question

--- src/problems/daily/factors.clj
+++ src/problems/daily/factors.clj
@@ -1,6 +1,35 @@
 (ns problems.daily.factors
   (:require [clojure.test :refer :all]))
 
+;; ====================
+;; Given a list of integers L, find the maximum length of a sequence of consecutive numbers that can be formed using elements from L.
+;; For example, given L = [5, 2, 99, 3, 4, 1, 100], return 5 as we can build a sequence [1, 2, 3, 4, 5] which has length 5.
+
+(defn inc_sub
+  "[int]->([int], [int])"
+  [xs]
+  ;; {:pred (< 0 (count xs))}
+  (loop [acc (take 1 xs) xs (drop 1 xs)]
+    (cond
+      (or (empty? xs)
+          (not= 1 (- (first xs) (first acc)))) [acc xs]
+      :else (recur (conj acc (first xs)) (rest xs)))))
+
+(defn max-len'
+  "[int]->int"
+  [xs]
+  (loop [xs xs acc 0]
+    (if (empty? xs)
+      acc
+      (let [[s xs] (inc_sub xs)]
+        (recur xs (max acc (count s)))))))
+
+(def max-len  (comp max-len' sort))
+
+
+;; ====================
+
+
 (def nats (lazy-seq (cons 1 (map + nats (repeat 1)))))
 
 (defn factors0 "int -> [int]"
@@ -13,7 +42,6 @@
   (filter (fn [x] (zero? (mod n x)))
           (range 2 (inc (Math/sqrt n)))))
 
-
 (defn ff [n x] ((comp not zero?) (mod n x)))
 
 (defn notfactored [n xs]
@@ -25,9 +53,20 @@
   (letfn [(pos-fact [n] (take-while #(< % (/ n 2)) primes0))]
     (let [odds (iterate #(+ 2 %) 3)]
       (lazy-seq (cons 2
-                    (filter #(notfactored % (pos-fact %)) odds))))))
+                      (filter #(notfactored % (pos-fact %)) odds))))))
 
 (deftest test-factors
+  (testing "inc_sub"
+    (is (= [[] []] (inc_sub [])))
+    (is (= [[2] [4]] (inc_sub [2 4])))
+    (is (= [[4 3 2] [6]] (inc_sub [2 3 4 6])))
+    (is (= 3 (max-len [2 3 4 6])))
+    (is (= 0 (max-len [])))
+    (is (= 5 (max-len [5, 2, 99, 3, 4, 1, 100]))))
+
+  (testing "isProbablePrime"
+    (is (not (.isProbablePrime (.subtract (.pow (biginteger 2) 51) (biginteger 1)) 5))))
+
   (testing "primes0"
     ;; (is (ff 4 2))
     ;; (is (ff 5 2))
@@ -40,9 +79,7 @@
 
     (is (= [2,3,5,7] (take 4 primes0)))
     (is (= [2,3,5,7,11,13,17,19,23,29,31] (take 11 primes0)))
-    (is (= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101] (take 26 primes0)))
-
-    )
+    (is (= [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101] (take 26 primes0))))
 
   (is (= (range 1 10) (take 9 nats)))
   (testing "factors0"
@@ -62,6 +99,18 @@
 (test-factors)
 
 (comment
+
+  (let [[x y & z] [1 2 3 4]]
+    [x y z])
+
+  (let [[x y & z] [1]]
+    [x y z])
+
+  (let [[x y] [1 2 3]]
+    [x y])
+
+  (< x nil)
+
   (every? #(< 5 %) [6 8])
   (factors 4)
   (def n 20)
