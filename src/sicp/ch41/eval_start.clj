5be6a4343d6b3cdeb7922b3ea171fcf567f0bf6a
vlad chub <vlad.chub@gmail.com>
2020-12-02 12:54:30 -0800

    test Exception Undefined Var

--- src/sicp/ch41/eval_start.clj
+++ src/sicp/ch41/eval_start.clj
@@ -6,16 +6,15 @@
 (defn evall [exp env]
   (let [type-handler (get-var :_exp-type env)
         etype (type-handler exp env)
-        handler (get @env etype)]
+        handler (get-var etype env)]
     ; (prn exp etype (type etype) (type handler))
-    (if handler
+    (if (not (nil? handler))
       (handler exp env)
-      (throw (Exception. (str "Unknow expression EVAL", exp))))))
+      (throw (Exception. (str "Undefined FN", etype))))))
 
 (def primitive-proc-symbols #{'+ '- '* '/ '< '> '= 'prn})
 
 (def primitives-env (into {} (map #(vector % (eval %)) primitive-proc-symbols)))
-
 
 (defn exp-type [exp env]
   (cond
@@ -24,8 +23,9 @@
         (and (seq? exp) (empty? exp))) :self-eval
     (symbol? exp) :symbol
     (not (nil? (primitives-env (first exp)))) :primitive-proc
-    (compound-proc? exp env) :compound-proc
-    :else (first exp)))
+    ; (compound-proc? exp env) :compound-proc
+    (and (seq? exp) (not (empty? exp))) (first exp)
+    :else (throw (Exception. (str "Unknow expression EVAL", exp)))))
 
 (defn eval-seq [exps env]
   (cond
@@ -40,34 +40,44 @@
   (swap! env assoc name val))
 
 (defn def-var [exp env]
-                      (set-var! (second exp) (evall (nth exp 2) env) env)
+  (set-var! (second exp) (evall (nth exp 2) env) env)
   ; (if-let [x (@env (second exp))]
   ;                       (throw (Exception. (str "VAR already defined ", (second exp))))
   ;                       (set-var! (second exp) (evall (nth exp 2) env) env))
   )
 
 (defn get-var [name env]
-  ; (prn "get-var" name (@env name))
-  (if (nil? @env)
-    (throw (Exception. (str "VAR not found ", name)))
+  (if-not (nil? @env)
     (if-let [x (@env name)]
       x
       (get-var name (@env :_next-env)))))
 
-(defn compound-proc? [exp env] false)
+(defn get-var [name env]
+  ; (prn "get-var" name (@env name))
+  (if (nil? env)
+    (throw (Exception. (str "Undefined VAR name: ", name)))
+    (if-let [x (@env name)]
+      x
+      (get-var name (@env :_next-env)))))
 
+(defn compound-proc? [exp env] (and (seq? exp) (not (nil? (get-var (first exp) env)))))
 
 (def fn-map
   {:_exp-type exp-type
    :self-eval (fn [exp env] exp)
-   :symbol get-var
+   :symbol (fn [exp env] (let [x (get-var exp env)]
+                           (if (nil? x)
+                             (throw (Exception. (str "Undefined VAR", name)))
+                             x)))
+
    'quote (fn [exp env] (second exp))
    'do (fn [exp env] (eval-seq (rest exp) env))
-   'def (fn [exp env] (if-let [x (@env (second exp))]
-                        (throw (Exception. (str "VAR already defined ", (second exp))))
-                        (set-var! (second exp) (evall (nth exp 2) env) env)))
+   'def (fn [exp env] (set-var! (second exp) (evall (nth exp 2) env) env))
+   ; 'def (fn [exp env] (if-let [x (@env (second exp))]
+   ;                      (throw (Exception. (str "VAR already defined ", (second exp))))
+   ;                      (set-var! (second exp) (evall (nth exp 2) env) env)))
    :primitive-proc (fn [exp env] (apply (@env (first exp)) (map (fn [exp] (evall exp env)) (rest exp))))
-   :compound-proc (fn [exp env] (apply (@env (first exp)) (map (fn [exp] (evall exp env)) (rest exp))))
+   ; :compound-proc (fn [exp env] (apply (@env (first exp)) (map (fn [exp] (evall exp env)) (rest exp))))
    })
 
 (def global-env (atom (merge primitives-env fn-map)))
--- test/sicp/ch41/eval_start_test.clj
+++ test/sicp/ch41/eval_start_test.clj
@@ -8,31 +8,33 @@
 ;   x)
 
 (deftest env-test
-  (let [ evall (fn[exp] (eval1/evall exp eval1/global-env)) ]
+  (let [evall (fn [exp] (eval1/evall exp eval1/global-env))]
     ; (prn @env)
     (testing "def and variable"
       (is (= 1 (evall '(do
                          (def x 1)
-                         x)))
-          )
-      ; (is (= 3 (evall '(do
-      ;                    (def x 1)
-      ;                    (def y 2)
-      ;                    (+ x y))))
-      ;     )
-      )
+                         x))))
+      (is (= 3 (evall '(do
+                         (def x 1)
+                         (def y 2)
+                         (+ x y)))))
+
+      (is (thrown? Exception
+                   (evall '(do
+                             (def x 1)
+                             (def y 2)
+                             (+ x z))))))
+
     (testing "primitives"
-     (is (= 1 (evall 1)))
-     (is (= "foo" (evall "foo")))
+      (is (= 1 (evall 1)))
+      (is (= "foo" (evall "foo")))
      ; (is (= 's (evall 's)))
-     (is (= 2 (evall (quote (+ 1 1)))))
-     (is (= 2 (evall '(+ 1 1))))
-     (is (= 2 (evall '(* 2 1))))
-     (is (= 1/2 (evall '(/ 1 2))))
-     (is (= true (evall '(< 1 2))))
-     (is (= true (evall '(= 1 1))))
-     (is (= true (evall '(= "a" "a"))))
-     (is (= false (evall '(= "b" "a"))))
-     (is (= 6 (evall '(* (+ 2 1) (/ 2 1)))))
-     )
-    ))
+      (is (= 2 (evall (quote (+ 1 1)))))
+      (is (= 2 (evall '(+ 1 1))))
+      (is (= 2 (evall '(* 2 1))))
+      (is (= 1/2 (evall '(/ 1 2))))
+      (is (= true (evall '(< 1 2))))
+      (is (= true (evall '(= 1 1))))
+      (is (= true (evall '(= "a" "a"))))
+      (is (= false (evall '(= "b" "a"))))
+      (is (= 6 (evall '(* (+ 2 1) (/ 2 1))))))))
